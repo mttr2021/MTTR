5ae3a1274183ea728e41bdf27bc326620597943e
Xiaohao Xu <65257938+JerryX1110@users.noreply.github.com>
2021-12-23 21:36:06 +0800

    Update dataset_preparation.sh

--- dataset_preparation.sh
+++ dataset_preparation.sh
@@ -1,34 +1,33 @@
-mkdir data
-cd data
-  # prepare Refer-YouTube-VOS
-  wget https://github.com/JerryX1110/awesome-rvos/blob/main/down_YTVOS_w_refer.py
-  python down_YTVOS_w_refer.py
 
-  # prepare a2d_sentences
-  mkdir a2d_sentences
-  cd a2d_sentences
-    wget https://web.eecs.umich.edu/~jjcorso/bigshare/A2D_main_1_0.tar.bz
-    tar jxvf A2D_main_1_0.tar.bz
-    mkdir text_annotations
-    cd text_annotations
-      wget https://kgavrilyuk.github.io/actor_action/a2d_annotation.txt
-      wget https://kgavrilyuk.github.io/actor_action/a2d_missed_videos.txt
-      wget https://github.com/JerryX1110/awesome-rvos/blob/main/down_a2d_annotation_with_instances.py
-      python down_a2d_annotation_with_instances.py
-      unzip a2d_annotation_with_instances.zip
-      #rm a2d_annotation_with_instances.zip
-    cd ..
-  cd ..
+# prepare Refer-YouTube-VOS
+wget https://github.com/JerryX1110/awesome-rvos/blob/main/down_YTVOS_w_refer.py
+python down_YTVOS_w_refer.py
 
-  # prepare jhmdb_sentences
-  mkdir jhmdb_sentences
-  cd jhmdb_sentences
-    wget http://files.is.tue.mpg.de/jhmdb/Rename_Images.tar.gz
-    wget https://kgavrilyuk.github.io/actor_action/jhmdb_annotation.txt
-    wget http://files.is.tue.mpg.de/jhmdb/puppet_mask.zip
-    tar -xzvf  Rename_Images.tar.gz
-    unzip puppet_mask.zip
+# prepare a2d_sentences
+mkdir a2d_sentences
+cd a2d_sentences
+  wget https://web.eecs.umich.edu/~jjcorso/bigshare/A2D_main_1_0.tar.bz
+  tar jxvf A2D_main_1_0.tar.bz
+  mkdir text_annotations
+  cd text_annotations
+    wget https://kgavrilyuk.github.io/actor_action/a2d_annotation.txt
+    wget https://kgavrilyuk.github.io/actor_action/a2d_missed_videos.txt
+    wget https://github.com/JerryX1110/awesome-rvos/blob/main/down_a2d_annotation_with_instances.py
+    python down_a2d_annotation_with_instances.py
+    unzip a2d_annotation_with_instances.zip
+    #rm a2d_annotation_with_instances.zip
   cd ..
 cd ..
+
+# prepare jhmdb_sentences
+mkdir jhmdb_sentences
+cd jhmdb_sentences
+  wget http://files.is.tue.mpg.de/jhmdb/Rename_Images.tar.gz
+  wget https://kgavrilyuk.github.io/actor_action/jhmdb_annotation.txt
+  wget http://files.is.tue.mpg.de/jhmdb/puppet_mask.zip
+  tar -xzvf  Rename_Images.tar.gz
+  unzip puppet_mask.zip
+cd ..
+
 
 echo "FINISHED!!"
